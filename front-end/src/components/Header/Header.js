7d21c9cbc5e2b1129824501568e254ee3931baf1
soumia elyakni <selyakni@gmail.com>
2022-11-01 21:53:17 +0100

    Add content to header component

--- front-end/src/components/Header/Header.js
+++ front-end/src/components/Header/Header.js
@@ -1,11 +1,22 @@
 import './Header.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+// import {'marhabaLogo.png'} from '../../../public/'
 
 const Header = ()=>{
     return (
     <>
-      <h1>Insert your infos</h1>
+      {/* <img src={"/${marhaba-logo}.png"} alt="image"></img> */}
+      <div className='header-container d-flex flex-row bg-info w-100 justify-content-evenly p-3'>
+        <button className='w-25'>
+            Login
+        </button>
+        <button className='w-25'>
+            Signin
+        </button>
+      </div>
     </>
     )
 }
 
-export default Header;
+export default Header 
