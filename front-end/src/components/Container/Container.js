cd2509d41958960a18721b8cb63d2b5988575bd7
soumia elyakni <selyakni@gmail.com>
2022-11-01 21:51:59 +0100

    update className of div

--- front-end/src/components/Container/Container.js
+++ front-end/src/components/Container/Container.js
@@ -1,10 +1,11 @@
 import './Container.css';
 import Login from './Login/Login';
 import Signup from './Signup/Signup';
+
 const Container = ()=>{
     return (
     <>
-    <div className='d-flex flex-row justify-content-between'>
+    <div id="ok" className='d-flex flex-row justify-content-evenly pt-3'>
       <Login/>
       <Signup/>
     </div>  
