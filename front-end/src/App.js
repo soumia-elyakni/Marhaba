ed98de4ea56aca027f42a01e662cc3eb7b904886
soumia elyakni <selyakni@gmail.com>
2022-11-03 14:39:53 +0100

    Clean import items

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import Header from './components/Header/Header';
-import  Container  from './components/Container/Container';
-import Footer from './components/Footer/Footer'
+
+import {Header, Container, Footer } from './components/index'
+
 const App = ()=>{
     return (
     <>
-      <Header/>
+
+      <Header></Header>
       <Container/>
       {/* <Footer/> */}
       
